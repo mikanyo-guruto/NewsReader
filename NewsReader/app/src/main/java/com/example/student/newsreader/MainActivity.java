059f51e72ae9b0bc8c4f1226e4e80673b00b2468
y-ohgi <ohgi.211@gmail.com>
2017-01-21 18:05:36 +0900

    merge

--- NewsReader/app/src/main/java/com/example/student/newsreader/MainActivity.java
+++ NewsReader/app/src/main/java/com/example/student/newsreader/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.student.newsreader;
 
-<<<<<<< HEAD
 import android.content.ClipData;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -35,40 +34,16 @@ public class MainActivity extends AppCompatActivity {
     @InjectView(R.id.items_list)
     ListView mItemsList;
 
-//    private ItemAdaptar adaptar;
-
-=======
-import android.content.Intent;
-import android.os.StrictMode;
-import android.support.v4.view.NestedScrollingChild;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.example.student.newsreader.Dto.CustomerDto;
-import com.example.student.newsreader.Dto.NewsDto;
-import com.example.student.newsreader.adapter.ReaderAdapter;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
->>>>>>> b9e809eaf8c46590f592a866035fea4d5173ffeb
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         ButterKnife.inject(this);
 
-//        final String[] foods = {"hoge", "fuga", "piyo"};
-//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+//  final String[] foods = {"hoge", "fuga", "piyo"};
+//  ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
 
-//        mItemsList.setAdapter(arrayAdapter);
+//  mItemsList.setAdapter(arrayAdapter);
 
         getItems();
 
@@ -123,31 +98,4 @@ public class MainActivity extends AppCompatActivity {
 //    if (mSwipeRefresh.isRefreshing()) {
 //        mSwipeRefresh.setRefreshing(false);
 //    }
-=======
-
-        final List<NewsDto> testLst = createTestCustomer();
-
-        ReaderAdapter adapter = new ReaderAdapter(this);
-
-        // Adapterへリストを追加
-        adapter.add(testLst);
-
-        ListView listview = (ListView) findViewById(R.id.NewsList);
-        listview.setAdapter(adapter);
-    }
-
-    // 20人分のテスト顧客データ　※本当はサーバから情報を取得するはず
-    private List<NewsDto> createTestCustomer(){
-
-        List<NewsDto> lst = new ArrayList<>();
-        for(int i = 1; i <= 20; i++) {
-            NewsDto dto = new NewsDto();
-            dto.setId(i);
-            dto.setName("Test" + i);
-            dto.setDate(new Date());
-            lst.add(dto);
-        }
-        return lst;
-    }
->>>>>>> b9e809eaf8c46590f592a866035fea4d5173ffeb
 }
